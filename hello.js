98e272e57224e9a8650edbaebacac58b645335fa
sreeteja19 <nannapaneni.sreeteja@gmail.com>
2015-12-08 22:33:22 +0000

    package.json updated

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@
   var MongoClient = require('mongodb').MongoClient;
       MongoClient.connect("mongodb://test:test123@ds027825.mongolab.com:27825/heroku_gcvgglf8",function(err, db){
         if(err) throw err;
-				
+	    var i = 1;
 	    db.collection('test').findOne({e_id:i}, function(err, doc){
         if(err) throw err;
         console.log("Server:"+doc);
