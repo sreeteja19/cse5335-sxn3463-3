b95b2d5df8c76c5ffa5b94190070cacfaa1bf96e
sreeteja19 <nannapaneni.sreeteja@gmail.com>
2015-12-08 22:38:40 +0000

    added service to retirbe data

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ app.get('/', function(request, response) {
 });
 
 app.get('/p/:tagId', function(req, res) {
-  var id= req.params.tagId;
-  
+  var i= req.params.tagId;
+  i = parseInt(i);
   //connection code
   
   var MongoClient = require('mongodb').MongoClient;
       MongoClient.connect("mongodb://test:test123@ds027825.mongolab.com:27825/heroku_gcvgglf8",function(err, db){
         if(err) throw err;
 				
-	    db.collection('test').findOne({e_id:id}, function(err, doc){
+	    db.collection('test').findOne({e_id:i}, function(err, doc){
         if(err) throw err;
         console.log("Server:"+doc);
 		
-		res.send(doc);
+		res.json(doc)
 	    db.close();
      });	 
 });
