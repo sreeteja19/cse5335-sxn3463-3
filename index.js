62b5d12c75cffedd91f1597d2c06a20fb67256d8
sreeteja19 <nannapaneni.sreeteja@gmail.com>
2015-12-08 22:06:40 +0000

    added url

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/p/:tagId', function(req, res) {
+  res.send("tagId is set to " + req.params.tagId);
+});
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
